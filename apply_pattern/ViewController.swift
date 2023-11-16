3c58a023897ce1cfb82eac881a5d9e46d6e34cd7
Brad MB <maskkwon@gmail.com>
2023-11-16 12:25:48 +0900

    [no] apply a pattern

--- apply_pattern/ViewController.swift
+++ apply_pattern/ViewController.swift
@@ -12,65 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet private weak var timeLabel: UILabel!
     @IBOutlet private weak var actionButton: UIButton!
 
-    private var startDate: Date?
-    private var stopDate: Date?
-    private var timer: Timer?
+    var state: State?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let presentor = Presentor(timeLabel: self.timeLabel, actionButton: self.actionButton)
+        self.state = InitState(presentor: presentor)
     }
 }
 
 extension ViewController {
     @IBAction private func touchActionButton() {
-        if self.startDate == nil {
-            self.startDate = Date()
-            self.scheduleTimer()
-            self.actionButton.setTitle("정지", for: .normal)
-
-        } else if self.stopDate == nil {
-            self.stopDate = Date()
-            self.invalidateTimer()
-            self.actionButton.setTitle("재시작", for: .normal)
-
-        } else if let stopDate = self.stopDate, let startDate = self.startDate {
-            let suspendedInterval = Date().timeIntervalSince(stopDate)
-            self.startDate = startDate.addingTimeInterval(suspendedInterval)
-            self.stopDate = nil
-            self.scheduleTimer()
-            self.actionButton.setTitle("정지", for: .normal)
-        }
-    }
-
-    private func scheduleTimer() {
-        self.timer = Timer.scheduledTimer(timeInterval: 0.01,
-                                          target: self,
-                                          selector: #selector(updateTimeLabel),
-                                          userInfo: nil,
-                                          repeats: true)
-    }
-
-    private func invalidateTimer() {
-        self.timer?.invalidate()
-        self.timer = nil
-    }
-
-    @objc private func updateTimeLabel() {
-        guard let startDate = self.startDate else {
-            self.timeLabel.text = "Time Label"
-            return
-        }
-
-        if let stopDate = self.stopDate {
-            let timeInterval = stopDate.timeIntervalSince(startDate)
-            let str = String(format: "%.2f", timeInterval)
-            self.timeLabel.text = "\(str) s"
-        } else {
-            let timeInterval = Date().timeIntervalSince(startDate)
-            let str = String(format: "%.2f", timeInterval)
-            self.timeLabel.text = "\(str) s"
-        }
+        self.state = self.state?.nextState
+        self.state?.updateUI()
     }
 }
 
